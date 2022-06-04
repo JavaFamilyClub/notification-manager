f0fb438372d4d9efaa88a11d20c662681db5340e
SunnyHe0123 <javafamily.club@outlook.com>
2022-06-04 18:58:34 +0800

    init feishu handler

--- feishu-notification/src/main/java/club/javafamily/nf/service/FeiShuNotifyHandler.java
+++ feishu-notification/src/main/java/club/javafamily/nf/service/FeiShuNotifyHandler.java
@@ -3,14 +3,19 @@ package club.javafamily.nf.service;
 import club.javafamily.nf.properties.FeiShuProperties;
 import club.javafamily.nf.request.FeiShuNotifyRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class FeiShuNotifyHandler implements NotifyHandler<FeiShuNotifyRequest> {
 
     private final FeiShuProperties feiShuProperties;
+    private final RestTemplate restTemplate;
 
-    public FeiShuNotifyHandler(FeiShuProperties feiShuProperties) {
+    public FeiShuNotifyHandler(FeiShuProperties feiShuProperties,
+                               RestTemplate restTemplate)
+    {
         this.feiShuProperties = feiShuProperties;
+        this.restTemplate = restTemplate;
     }
 
     @Override
